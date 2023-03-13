4d18c333b57ac45e2703d8701bf835052203d50a
gremturm79 <gremturm@gmail.com>
2023-03-13 20:27:13 +0100

    add homework1

--- homework/pars.py
+++ homework/pars.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = requests.get('https://klops.ru')
 soup = BeautifulSoup(url.text, 'html.parser')
@@ -9,9 +10,24 @@ soup2 = soup.findAll(class_='item')
 klops_title = ''
 klops_news = ''
 lst = []
-lst1 = []
+lst_href = []
 i = 0
 while i < 5:  # 35 all elements
     lst.append(soup1[i + 1].text)
-    lst1.append(soup2[i].get('href'))
+    lst_href.append(soup2[i].get('href'))
     i += 1
+lst_img = []
+reg = r"[\w+]+://\d\.\w+\.\w+\.\w+/\w+/\w+\?*=*\?*\w+=*\w+"
+for i in lst_href:
+    url1 = requests.get(i)
+    soup1 = BeautifulSoup(url1.text, 'html.parser')
+    soup3 = soup1.findAll('link', {'itemprop': 'image'})
+    res = re.findall(reg, str(soup3))
+    lst_img.append(res)
+
+lst_image = []
+for i in range(len(lst_img)):
+    lst_image.append(lst_img[i][0])
+
+
+
---
+++ homework1/fl1.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, url_for, session, request
+from flask_sqlalchemy import SQLAlchemy, query
+# Через библиотеку sqlite3 не получилось сделать дом.работу получаю исключения и статьи не добавляются в БД  пока не
+# нашёл причины почему и сделал через библиотеку Flask-SQLAlchemy, здесь код работает
+# Traceback (most recent call last):
+# File "C:\Users\Alex\AppData\Local\Programs\Python\Python310\lib\code.py", line 90, in runcode
+# exec(code, self.locals)
+# File "<input>", line 1, in <module>
+# NameError: name 'create_db' is not defined
+
+# конфигурация для работы с БД
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):  # при запуске файла создаётся таблица
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    url = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'Article{self.id}'
+
+
+menu = [{'name': 'Главная', 'url': 'index'},
+        {'name': 'Новости', 'url': 'news'}
+        ]
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Главная', menu=menu, model=Article.query.all())
+
+
+@app.route('/news')
+def news():
+    return render_template('news.html', title='Новости', menu=menu, model=Article.query.all())
+
+
+# добавляет url и другие поля, но так как url прописан в функции url_for() base.html выдаётся ошибка т.к такой путь
+# ещё не прописан в декораторе app.route(), поэтому пока данные не желательно заносить
+
+
+@app.route('/add_bd', methods=['GET', 'POST'])
+def add_bd():
+    if request.method == 'POST':
+        rec_bd = Article(id=request.form['id'], title=request.form['name'], url=request.form['url'])
+        db.session.add(rec_bd)
+        db.session.flash()
+        db.session.commit()
+    return render_template('add_bd.html', title='Добавление в базу данных', menu=menu, model=Article.query.all())
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+    app.run(debug=True)
