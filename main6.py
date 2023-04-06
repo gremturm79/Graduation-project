d8b2d32fbe3c99a5a891b8992d853368e0a001f3
gremturm79 <gremturm@gmail.com>
2023-04-06 21:34:58 +0200

    main6

--- main6.py
+++
@@ -1,8 +0,0 @@
-def info(**kwargs):
-    my_dict.update(kwargs)
-    return my_dict
-
-
-my_dict = {'first': 'one'}
-print(info(k1=22, k2=31, k3=11, k4=91))
-print(info(name='Bob', age=31, weight=61, eyes_color='grey'))
