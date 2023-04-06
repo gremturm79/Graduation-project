41f7654771306fba5dc809103e36b463cdcccc9d
gremturm79 <gremturm@gmail.com>
2023-04-06 15:57:06 +0200

    lesson11

--- lesson11.py
+++
@@ -1,8 +0,0 @@
-lst = [int(input('->')) for i in range(int(input('Введите количество элементов')))]
-print(lst)
-ch = int(input('Введите число: '))
-for i in range(len(lst)):
-    if lst[i] == ch:
-        print(ch)
-
-
