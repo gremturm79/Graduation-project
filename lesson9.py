f6656b98fde624ab71bbfb9cba309e8073fc56aa
gremturm79 <gremturm@gmail.com>
2023-04-06 21:29:42 +0200

    lesson9

--- lesson9.py
+++
@@ -1,6 +0,0 @@
-a = [int(input('->')) for i in range(int(input('Введите количество элементов:  ')))]
-ch = int(input('Введите число: '))
-print('ch =', ch)
-for i in a:
-    if i == ch:
-        print('Число', ch, 'присутствует в элементах списка')
