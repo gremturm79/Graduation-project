4e7d6591687fc28ca71cd4a967fedf71e9336f66
gremturm79 <gremturm@gmail.com>
2023-04-06 16:41:49 +0200

    lesson37

--- lesson37.py
+++
@@ -1,9 +0,0 @@
-lst = [
-    {'name': 'Jenifer', 'final': 95},
-    {'name': 'David', 'final': 92},
-    {'name': 'Nicolas', 'final': 98}
-]
-res = sorted(lst, key=lambda item: item['name'])
-print(res)
-res1 = sorted(lst, key=lambda item: item['final'], reverse=True)
-print(res1)
