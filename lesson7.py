ae673942ab0eb5ac28d774a11552cabf02f7bb52
gremturm79 <gremturm@gmail.com>
2023-04-06 21:26:36 +0200

    lesson7

--- lesson7.py
+++
@@ -1,4 +0,0 @@
-a = [int(input('Введите значение элемента: ')) for i in range(int(input('Введите количество элементов: ')))]
-for i in range(len(a)):
-    if a[i] > a[i-1]:
-        print(a[i], end=' ')
