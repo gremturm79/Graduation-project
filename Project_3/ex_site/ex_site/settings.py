fcff747b279bc56a96dae19f8e39987ebc916059
gremturm79 <gremturm@gmail.com>
2023-05-24 12:21:09 +0200

    'add_os.env'

--- Project_3/ex_site/ex_site/settings.py
+++ Project_3/ex_site/ex_site/settings.py
@@ -36,11 +36,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = 'eptzqqawrupifnhy'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'logvinov.al.m@gmail.com'
-# MY_PASSWORD_MAIL = os.getenv('MY_PASSWORD_MAIL')
 MY_HOST_MAIL = ''
 # Application definition
 
